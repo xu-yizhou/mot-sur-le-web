670796e21775b5abd94088c0753c9ff126148cad
yizhou <yizhou.xu8859@gmail.com>
2019-02-03 14:02:12 +0100

    init env

---
+++ prepare-environnement-projet.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+#-------------------------------------------------------------------------------
+# avant de lancer ce script on se place dans le repertoire du projet
+# par exemple : 
+# cd Documents/20172018-S1/Projet_Encadre
+# mkdir PROJET_MOT_SUR_LE_WEB
+# cd PROJET_MOT_SUR_LE_WEB
+# on peut desormais creer l'arborescence de travail
+#-------------------------------------------------------------------------------
+mkdir ./CONTEXTES;
+mkdir ./DUMP-TEXT;
+mkdir ./IMAGES;
+mkdir ./PAGES-ASPIREES;
+mkdir ./PROGRAMMES;
+mkdir ./TABLEAUX;
+mkdir ./URLS;
+# le dossier URLS contiendra le fichier initial d'URLs 
